9470576b590904bd7e43d3216ae8be88ea8dc367
Eran Rundstein <e@keves.org>
2013-12-20 21:32:11 -0800

    wip

--- src/sketch.ino
+++ src/sketch.ino
@@ -4,24 +4,95 @@
 #define FORCE_SOFTWARE_PINS
 #include "FastSPI_LED2.h"
 
-#define NUM_LEDS 150
+
+#define SEG1_BEGIN 0
+#define SEG1_END 26
+#define SEG2_BEGIN 27
+#define SEG2_END 50
+#define SEG3_BEGIN 51
+#define SEG3_END 75
+#define SEG4_BEGIN 76
+#define SEG4_END 106
+
+#define NUM_LEDS 106
 #define DATA_PIN 6
 
 CRGB leds[NUM_LEDS];
 
 void setup()
 {
-    delay(2000);
-
-    FastLED.addLeds<UCS1903, DATA_PIN, GRB>(leds, NUM_LEDS);
+    FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+    FastLED.setBrightness(128);
     pinMode(LED_PIN, OUTPUT);
+
+    for (int i = 0; i < NUM_LEDS; ++i) {
+        leds[i] = CRGB::Red;
+    }
+    FastLED.show();
 }
+
+
+
+unsigned long tick = 0;
+uint16_t i, j, x, y;
+uint32_t c, d;
+
+uint32_t Wheel(uint16_t WheelPos)
+{
+  byte r, g, b;
+  switch(WheelPos / 128)
+  {
+    case 0:
+      r = 127 - WheelPos % 128; // red down
+      g = WheelPos % 128;       // green up
+      b = 0;                    // blue off
+      break;
+    case 1:
+      g = 127 - WheelPos % 128; // green down
+      b = WheelPos % 128;       // blue up
+      r = 0;                    // red off
+      break;
+    case 2:
+      b = 127 - WheelPos % 128; // blue down
+      r = WheelPos % 128;       // red up
+      g = 0;                    // green off
+      break;
+  }
+  return(CRGB(r,g,b));
+}
+
+void animate() {
+    j = tick % 384;
+    for (i=0; i <NUM_LEDS; i++) {
+        leds[i] =  Wheel(((i * 384 / NUM_LEDS * (2)) + j) % 384);
+    }
+    FastLED.show();
+}
+
+void loop() {
+    tick++;
+    animate();
+    //handleButtons();
+}
+
 
 // This function runs over and over, and is where you do the magic to light
 // your leds.
-void loop() {
+void xxloop() {
+    delay(700);
+    for (int i = SEG4_BEGIN; i < SEG4_END; ++i) {
+        leds[i] = CRGB::Green;
+    }
+    FastLED.show();
+    delay(700);
+    for (int i = SEG4_BEGIN; i < SEG4_END; ++i) {
+        leds[i] = CRGB::Red;
+    }
+    FastLED.show();
+
+
    // Move a single white led
-   for(int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1) {
+/*   for(int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1) {
       // Turn our current led on to white, then show the leds
       leds[whiteLed] = CRGB::Red;
       leds[whiteLed+1] = CRGB::Green;
@@ -31,11 +102,11 @@ void loop() {
       FastLED.show();
 
       // Wait a little bit
-      delay(1000);
+      delay(10);
 
       // Turn our current led back to black for the next loop around
       leds[whiteLed] = CRGB::Black;
-   }
+   }*/
 }
 
 /*void loop()
