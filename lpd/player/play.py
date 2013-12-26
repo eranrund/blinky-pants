c05f071a7a90f5d566a49cc212bd962f631e76cb
Eran Rundstein <e@keves.org>
2013-12-25 23:00:22 -0800

    wip

--- lpd/player/play.py
+++ lpd/player/play.py
@@ -30,7 +30,7 @@ samplerate = 44100
 
 import udp_client
 
-def calculate_levels(data):
+def calculate_levels(data, n_levels):
  
     # Convert raw sound data to Numpy array
     fmt = "%dH"%(len(data)/2)
@@ -53,10 +53,13 @@ def calculate_levels(data):
  
     # Add up for 6 lights
     #levels = [sum(fourier[i:(i+size/6)]) for i in xrange(0, size, size/6)][:6]
-    levels = [sum(fourier[i:(i+size/10)]) for i in xrange(0, size, size/10)][:10]
+    levels = [sum(fourier[i:(i+size/n_levels)]) for i in xrange(0, size, size/n_levels)][:n_levels]
     
     return levels
 
+from udp_client import send_buf2,hsv,N_LEDS
+n_levels = 16
+old_levels = [0] * n_levels
 while data != '':
     out = []
     for i in range(0, len(data), 4):
@@ -68,7 +71,8 @@ while data != '':
     #level = int(level * 255)
     #print level    
 
-    levels = calculate_levels(''.join(out))     
+    levels = calculate_levels(''.join(out), n_levels)     
+    scale = 20
     for level in levels:
         level = max(min(level / scale, 1.0), 0.0)
         level = level**exponent 
@@ -79,20 +83,65 @@ while data != '':
 
     num_segs = len(levels)
     leds_per_seg = udp_client.N_LEDS / num_segs
-    
-    buf = []
-    led_cnt = 0
-    for level in levels:
-       level = max(min(level / scale, 1.0), 0.0)
-       level = level**exponent 
-       level = int(level * 255)
-       for led in range(leds_per_seg):
-            buf.append(udp_client.hsv(led_cnt, 255, level))
-            led_cnt += 1
 
-    while len(buf) < udp_client.N_LEDS:
-        buf.append([0,0,0])
-    udp_client.send_buf2(reversed(buf))
+    levelz = []
+    for level in levels:
+        level = max(min(level / scale, 1.0), 0.0)
+        level = level**exponent 
+        level = int(level * 255)
+        levelz.append(level)
+    levels = levelz
+
+    # compare to old
+    for i, new_level in enumerate(levels):
+        if new_level > old_levels[i]:
+            old_levels[i] = new_level
+        else:
+            level = (old_levels[i] * 7 + new_level) / 8
+            old_levels[i] = level
+            levels[i] = level
+
+
+    center_idx = udp_client.N_LEDS / 2
+    hue_min = 0
+    hue_max = 60
+    buf = [[0,0,0]] * udp_client.N_LEDS
+    leds_per_seg = 10
+    idx_left = center_idx
+    idx_right = center_idx
+    for level_idx, level in enumerate(levels):
+        h = level_idx * leds_per_seg / 2
+        for i in range(leds_per_seg / 2):            
+            c = hsv(h, 255, level)
+            buf[idx_left] = c
+            buf[idx_right] = c
+
+            idx_left -= 1
+            idx_right += 1
+
+#        idx_from = center_idx - (((level_idx + 1) * leds_per_seg) / 2)
+#        idx_to = 
+
+
+        #buf[i + center_idx] = hsv(60, 255, level)
+
+
+    send_buf2(buf)
+
+    
+    # buf = []
+    # led_cnt = 0
+    # for level in levels:
+    #    level = max(min(level / scale, 1.0), 0.0)
+    #    level = level**exponent 
+    #    level = int(level * 255)
+    #    for led in range(leds_per_seg):
+    #         buf.append(udp_client.hsv(led_cnt, 255, level))
+    #         led_cnt += 1
+
+    # while len(buf) < udp_client.N_LEDS:
+    #     buf.append([0,0,0])
+    # udp_client.send_buf2(reversed(buf))
 
     stream.write(data)
     data = wf.readframes(CHUNK)
