e049772c967b62b96455cff094dc1503f92e32b1
Eran Rundstein <e@keves.org>
2014-05-18 20:12:44 -0700

    vest audio failed fft

--- vest/src/sketch.ino
+++ vest/src/sketch.ino
@@ -1,3 +1,6 @@
+#define ARM_MATH_CM4
+#include <arm_math.h>
+
 #include "FastLED.h"
 #include <EEPROM.h>
 #include "BlinkyCommon.h"
@@ -90,6 +93,30 @@ bool g_ext_ctrl_board_enable = false;
 
 //
 unsigned int seed = 0;
+
+
+
+int SAMPLE_RATE_HZ = 9000;             // Sample rate of the audio in hertz.
+float SPECTRUM_MIN_DB = 30.0;          // Audio intensity (in decibels) that maps to low LED brightness.
+float SPECTRUM_MAX_DB = 60.0;          // Audio intensity (in decibels) that maps to high LED brightness.
+const int FFT_SIZE = 256;              // Size of the FFT.  Realistically can only be at most 256 
+                                       // without running out of memory for buffers and other state.
+const int AUDIO_INPUT_PIN = 17;        // Input ADC pin for audio data.
+const int ANALOG_READ_RESOLUTION = 10; // Bits of resolution for the ADC.
+const int ANALOG_READ_AVERAGING = 16;  // Number of samples to average with each ADC reading.
+const int NEO_PIXEL_COUNT = S1_LEN;         // Number of neo pixels.  You should be able to increase this without
+                                       // any other changes to the program.
+
+IntervalTimer samplingTimer;
+float samples[FFT_SIZE*2];
+float magnitudes[FFT_SIZE];
+int sampleCounter = 0;
+float frequencyWindow[NEO_PIXEL_COUNT+1];
+float hues[NEO_PIXEL_COUNT];
+void spectrumSetup();
+void spectrumLoop();
+void samplingBegin();
+boolean samplingIsDone() ;
 
 
 
@@ -278,6 +305,11 @@ void setup()
     delay(50);
     g_ext_ctrl_board_enable = digitalRead(3) == 0;
 
+
+    pinMode(AUDIO_INPUT_PIN, INPUT);
+    analogReadResolution(ANALOG_READ_RESOLUTION);
+    analogReadAveraging(ANALOG_READ_AVERAGING);
+
    pinMode(18, INPUT_PULLUP);
    pinMode(19, INPUT_PULLUP);
   
@@ -325,11 +357,32 @@ delay(1000);
 //}
     
     set_default_patterns();
+
+
+    spectrumSetup();
+    samplingBegin();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // Loop
 ////////////////////////////////////////////////////////////////////////////////
+void fft_loop() {
+  // Calculate FFT if a full sample is available.
+  if (samplingIsDone()) {
+    // Run FFT on sample data.
+    arm_cfft_radix4_instance_f32 fft_inst;
+    arm_cfft_radix4_init_f32(&fft_inst, FFT_SIZE, 0, 1);
+    arm_cfft_radix4_f32(&fft_inst, samples);
+    // Calculate magnitude of complex numbers output by the FFT.
+    arm_cmplx_mag_f32(samples, magnitudes, FFT_SIZE);
+  
+      spectrumLoop();
+  
+    // Restart audio sampling.
+    samplingBegin();
+  }
+    
+}
 
 inline void loop_brightness()
 {
@@ -419,6 +472,9 @@ void loop_buttons() {
 
 void loop() {
     loop_brightness();
+    fft_loop();
+    return;
+
     loop_serial();
     loop_speed();    
     loop_buttons();
@@ -440,4 +496,146 @@ void efx_blink(int h, int repeats) {
             FastLED.show();
         }
     }
+}
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// UTILITY FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+// Compute the average magnitude of a target frequency window vs. all other frequencies.
+void windowMean(float* magnitudes, int lowBin, int highBin, float* windowMean, float* otherMean) {
+    *windowMean = 0;
+    *otherMean = 0;
+    // Notice the first magnitude bin is skipped because it represents the
+    // average power of the signal.
+    for (int i = 1; i < FFT_SIZE/2; ++i) {
+      if (i >= lowBin && i <= highBin) {
+        *windowMean += magnitudes[i];
+      }
+      else {
+        *otherMean += magnitudes[i];
+      }
+    }
+    *windowMean /= (highBin - lowBin) + 1;
+    *otherMean /= (FFT_SIZE / 2 - (highBin - lowBin));
+}
+
+// Convert a frequency to the appropriate FFT bin it will fall within.
+int frequencyToBin(float frequency) {
+  float binFrequency = float(SAMPLE_RATE_HZ) / float(FFT_SIZE);
+  return int(frequency / binFrequency);
+}
+
+// Convert from HSV values (in floating point 0 to 1.0) to RGB colors usable
+// by neo pixel functions.
+CRGB pixelHSVtoRGBColor(float hue, float saturation, float value) {
+  // Implemented from algorithm at http://en.wikipedia.org/wiki/HSL_and_HSV#From_HSV
+  float chroma = value * saturation;
+  float h1 = float(hue)/60.0;
+  float x = chroma*(1.0-fabs(fmod(h1, 2.0)-1.0));
+  float r = 0;
+  float g = 0;
+  float b = 0;
+  if (h1 < 1.0) {
+    r = chroma;
+    g = x;
+  }
+  else if (h1 < 2.0) {
+    r = x;
+    g = chroma;
+  }
+  else if (h1 < 3.0) {
+    g = chroma;
+    b = x;
+  }
+  else if (h1 < 4.0) {
+    g = x;
+    b = chroma;
+  }
+  else if (h1 < 5.0) {
+    r = x;
+    b = chroma;
+  }
+  else // h1 <= 6.0
+  {
+    r = chroma;
+    b = x;
+  }
+  float m = value - chroma;
+  r += m;
+  g += m;
+  b += m;
+  return CRGB(int(255*r), int(255*g), int(255*b));
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// SPECTRUM DISPLAY FUNCTIONS
+///////////////////////////////////////////////////////////////////////////////
+
+void spectrumSetup() {
+  // Set the frequency window values by evenly dividing the possible frequency
+  // spectrum across the number of neo pixels.
+  float windowSize = (SAMPLE_RATE_HZ / 2.0) / float(NEO_PIXEL_COUNT);
+  for (int i = 0; i < NEO_PIXEL_COUNT+1; ++i) {
+    frequencyWindow[i] = i*windowSize;
+  }
+  // Evenly spread hues across all pixels.
+  for (int i = 0; i < NEO_PIXEL_COUNT; ++i) {
+    hues[i] = 360.0*(float(i)/float(NEO_PIXEL_COUNT-1));
+  }
+}
+
+void spectrumLoop() {
+  // Update each LED based on the intensity of the audio 
+  // in the associated frequency window.
+  float intensity, otherMean;
+  for (int i = 0; i < NEO_PIXEL_COUNT; ++i) {
+    windowMean(magnitudes, 
+               frequencyToBin(frequencyWindow[i]),
+               frequencyToBin(frequencyWindow[i+1]),
+               &intensity,
+               &otherMean);
+    // Convert intensity to decibels.
+    intensity = 20.0*log10(intensity);
+    // Scale the intensity and clamp between 0 and 1.0.
+    intensity -= SPECTRUM_MIN_DB;
+    intensity = intensity < 0.0 ? 0.0 : intensity;
+    intensity /= (SPECTRUM_MAX_DB-SPECTRUM_MIN_DB);
+    intensity = intensity > 1.0 ? 1.0 : intensity;
+    leds[i] = pixelHSVtoRGBColor(hues[i], 1.0, intensity);
+  }
+  FastLED.show();
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// SAMPLING FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+void samplingCallback() {
+  // Read from the ADC and store the sample data
+  samples[sampleCounter] = (float32_t)analogRead(AUDIO_INPUT_PIN);
+  Serial.println(samples[sampleCounter]);
+  // Complex FFT functions require a coefficient for the imaginary part of the input.
+  // Since we only have real data, set this coefficient to zero.
+  samples[sampleCounter+1] = 0.0;
+  // Update sample buffer position and stop after the buffer is filled
+  sampleCounter += 2;
+  if (sampleCounter >= FFT_SIZE*2) {
+    samplingTimer.end();
+  }
+}
+
+void samplingBegin() {
+  // Reset sample buffer position and start callback at necessary rate.
+  sampleCounter = 0;
+  samplingTimer.begin(samplingCallback, 1000000/SAMPLE_RATE_HZ);
+}
+
+boolean samplingIsDone() {
+  return sampleCounter >= FFT_SIZE*2;
 }
