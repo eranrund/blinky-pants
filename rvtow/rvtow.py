4d7bebea675a9181dc2b0334add5a52f6993905b
Eran Rundstein <e@keves.org>
2014-06-24 14:03:49 -0700

    fix rvrow

--- rvtow/rvtow.py
+++ rvtow/rvtow.py
@@ -10,13 +10,14 @@ try:
     page = lxml.html.fromstring(r.text)
 
     txt = page.cssselect('div#primary > p')[0].text_content()
-    assert '2 vehicles were found matching' in txt
+    assert '3 vehicles were found matching' in txt, txt
 
     trs = page.cssselect('table.searchresults tr.heading')
-    assert len(trs) == 2
+    assert len(trs) == 3, len(trs)
 
-    assert trs[0].cssselect('td')[-1].text_content().encode('ascii', 'ignore') == '2/4/149:54 AM'
-    assert trs[1].cssselect('td')[-1].text_content().encode('ascii', 'ignore') == '10/19/1311:48 PM'
+    assert trs[0].cssselect('td')[-1].text_content().encode('ascii', 'ignore') == '6/20/144:53 PM',trs[0].cssselect('td')[-1].text_content().encode('ascii', 'ignore')
+    assert trs[1].cssselect('td')[-1].text_content().encode('ascii', 'ignore') == '2/4/149:54 AM',trs[1].cssselect('td')[-1].text_content().encode('ascii', 'ignore')
+    assert trs[2].cssselect('td')[-1].text_content().encode('ascii', 'ignore') == '10/19/1311:48 PM', trs[2].cssselect('td')[-1].text_content().encode('ascii', 'ignore')
 
 
     r = requests.post('http://wmq.etimspayments.com/pbw/inputAction.doh', data={
@@ -34,20 +35,31 @@ try:
 
     el = page.cssselect('li.error')
     assert len(el) == 1
-    assert el[0].text_content().encode('ascii', 'ignore').strip() == 'The Plate entered has a balance of $0.00x'
+    assert el[0].text_content().encode('ascii', 'ignore').strip() == 'The Plate entered has a balance of $0.00'
 except:
     import traceback; traceback.print_exc()
-    msg = MIMEText('CHECK FAILED !!!!')
-    msg['Subject'] = 'RV CHECKER - FAILED !!!!'
-    msg['From'] = 'rv.omercy@gmail.com'
-    msg['To'] = 'eranrund@gmail.com, shlomo.zippel@gmail.com'
-else:
-    msg = MIMEText('CHECK OK')
-    msg['Subject'] = 'RV CHECKER - OKAY'
-    msg['From'] = 'rv.omercy@gmail.com'
-    msg['To'] = 'eranrund@gmail.com, shlomo.zippel@gmail.com'
+    msg1 = MIMEText('CHECK FAILED !!!!')
+    msg1['Subject'] = 'RV CHECKER - FAILED !!!!'
+    msg1['From'] = 'rv.omercy@gmail.com'
+    msg1['To'] = 'eranrund@gmail.com'
 
+    msg2 = MIMEText('CHECK FAILED !!!!')
+    msg2['Subject'] = 'RV CHECKER - FAILED !!!!'
+    msg2['From'] = 'rv.omercy@gmail.com'
+    msg2['To'] = 'shlomo.zippel@gmail.com'
+
+else:
+    msg1 = MIMEText('CHECK OK')
+    msg1['Subject'] = 'RV CHECKER - OKAY'
+    msg1['From'] = 'rv.omercy@gmail.com'
+    msg1['To'] = 'eranrund@gmail.com'
+
+    msg2 = MIMEText('CHECK OK')
+    msg2['Subject'] = 'RV CHECKER - OKAY'
+    msg2['From'] = 'rv.omercy@gmail.com'
+    msg2['To'] = 'shlomo.zippel@gmail.com'
 
 s = smtplib.SMTP('localhost')
-s.sendmail(msg['From'], msg['To'], msg.as_string())
+s.sendmail(msg1['From'], msg1['To'], msg1.as_string())
+s.sendmail(msg2['From'], msg2['To'], msg2.as_string())
 s.quit()
